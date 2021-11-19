dd6b5dd22538365f52f23b24337e9b34c925a6d4
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2021-11-19 16:20:15 +0530

    Updated the tests

--- src/test/java/GetListOfUsers.java
+++ src/test/java/GetListOfUsers.java
@@ -1,9 +1,6 @@
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.comparesEqualTo;
 
 public class GetListOfUsers {
