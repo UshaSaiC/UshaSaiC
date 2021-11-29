444d8740b610aaa243added724d81f6a514257d0
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2021-11-29 07:23:05 +0530

    Updated tests

--- src/test/java/CreateAUser.java
+++ src/test/java/CreateAUser.java
@@ -1,4 +1,8 @@
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -7,15 +11,20 @@ public class CreateAUser {
 
     @Test
     public void testCreatingAUserAndValidatingResponseCodeAndLoggingResponseBody() {
-         String requestBody = "{\n" +
-                "  \"name\": \"test\",\n" +
-                "  \"job\": \"testing\" \n}";
+        JSONObject json = new JSONObject();
+        json.put("name", "test");
+        json.put("job", "testing");
 
-        Response response = given().body(requestBody).
+        Response response = given().body(json.toJSONString()).contentType(ContentType.JSON).
                 when().post("https://reqres.in/api/users").
                 then().assertThat().statusCode(201).
                 and().extract().response();
-        System.out.println("Details of user created : "+response.asString());
+
+        System.out.println("Details of user created : "+response.asPrettyString());
+
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        String id = jsonPathEvaluator.get("id");
+        System.out.println("ID of user created " + id);
     }
 
 }
--- src/test/java/GetListOfUsers.java
+++ src/test/java/GetListOfUsers.java
@@ -1,7 +1,10 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class GetListOfUsers {
 
@@ -11,5 +14,17 @@ public class GetListOfUsers {
         when().get("https://reqres.in/api/users").
         then().assertThat().statusCode(200).
         body("per_page", comparesEqualTo(6));
+    }
+
+    @Test
+    public void testGetListOfUsers() {
+        int pageNo = 1;
+        Response response = given().contentType(ContentType.JSON).queryParam("page", pageNo).
+                when().get("https://reqres.in/api/users").
+                then().assertThat().statusCode(200).
+                body("data", hasSize(6)).
+                body("per_page", comparesEqualTo(6)).
+                and().extract().response();
+        System.out.println("response of the list of users API is : " + response.asPrettyString());
     }
 }
